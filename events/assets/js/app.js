144933d07edfe577e933bd59c919de08409b681f
Luis Ortega <luisroberto.ortega6@gmail.com>
2024-05-24 21:28:17 -0600

    Changed event added to scripts

--- events/assets/js/app.js
+++ events/assets/js/app.js
@@ -1,88 +1,110 @@
-const button = document.querySelector('.btn.btn-primary');
-const title = document.querySelector('.display-2');
-const description = document.querySelector('.description-container');
+document.addEventListener("DOMContentLoaded", () => {
+    
+  const button = document.querySelector(".btn.btn-primary");
+  const title = document.querySelector(".display-2");
+  const description = document.querySelector(".description-container");
 
-// // Usando las constantes para ocultar el texto
-// button.addEventListener('click', (e) => {
-//     if (title.style.display != 'none') {
-//         title.style.display = 'none';
-//         description.style.display = 'none';
-//         button.textContent = "Mostrar";
-//     } else {
-//         title.style.display = 'block';
-//         description.style.display = 'block';
-//         button.textContent = "Ocultar";
-//     }
-// });
+  // // Usando las constantes para ocultar el texto
+  // button.addEventListener('click', (e) => {
+  //     if (title.style.display != 'none') {
+  //         title.style.display = 'none';
+  //         description.style.display = 'none';
+  //         button.textContent = "Mostrar";
+  //     } else {
+  //         title.style.display = 'block';
+  //         description.style.display = 'block';
+  //         button.textContent = "Ocultar";
+  //     }
+  // });
 
-// Usando el evento (e.target), a dónde apunta
-const list = document.querySelectorAll('.list-group-item');
+  // Usando el evento (e.target), a dónde apunta
+  const list = document.querySelectorAll(".list-group-item");
 
-list.forEach((item) => {
-    item.addEventListener('click', (e) => {
-        if (item.style.display !== 'none') {
-            e.target.style.display = 'none';
-        }
+  list.forEach((item) => {
+    item.addEventListener("click", (e) => {
+      if (item.style.display !== "none") {
+        e.target.style.display = "none";
+      }
     });
 
     setInterval(() => {
-        list.forEach((item) => {
-          if (item.style.display === 'none') {
-            item.style.display = 'block';
-          }
-        });
-      }, 1000);
-});
+      list.forEach((item) => {
+        if (item.style.display === "none") {
+          item.style.display = "block";
+        }
+      });
+    }, 1000);
+  });
 
-// Usando el contexto .this
-const footer = document.querySelector('.text-footer');
+  // Usando el contexto .this
+  const footer = document.querySelector(".text-footer");
 
-footer.addEventListener('click', function (e) { // this solo trabaja con funciones tradicionales
-    if (footer.style.display != 'none') {
-        this.style.display = 'none';
+  footer.addEventListener("click", function (e) {
+    // this solo trabaja con funciones tradicionales
+    if (footer.style.display != "none") {
+      this.style.display = "none";
     }
 
     setTimeout(() => {
-        if (this.style.display = 'none') {
-            this.style.display = 'block';
-        }
+      if ((this.style.display = "none")) {
+        this.style.display = "block";
+      }
     }, 2000);
-});
+  });
 
-button.addEventListener('mouseenter', function (e) {
-    this.className = 'btn btn-success';
-});
+  button.addEventListener("mouseenter", function (e) {
+    this.className = "btn btn-success";
+  });
 
-button.addEventListener('mouseleave', function (e) {
-    this.className = 'btn btn-primary';
-});
+  button.addEventListener("mouseleave", function (e) {
+    this.className = "btn btn-primary";
+  });
 
-// Usando TimeOut()
-setTimeout((p1, p2, p3) => {
-    // console.log('Hello World! - 2 seconds timeout');
-    // console.log(p1 + p2);
-    // console.log(p2 + p3);
-    // console.log(p3 + p1);
-}, 2000, 2, 4, 6);
+  // Usando TimeOut()
+  setTimeout(
+    (p1, p2, p3) => {
+      // console.log('Hello World! - 2 seconds timeout');
+      // console.log(p1 + p2);
+      // console.log(p2 + p3);
+      // console.log(p3 + p1);
+    },
+    2000,
+    2,
+    4,
+    6
+  );
 
-// const input = document.getElementById('input');
+  // const input = document.getElementById('input');
 
-// input.addEventListener('keydown', function (e) {
-//     console.log("Tecla presionada: " + e.key);
-// });
+  // input.addEventListener('keydown', function (e) {
+  //     console.log("Tecla presionada: " + e.key);
+  // });
 
-// input.addEventListener('keyup', function (e) {
-//     console.log("Tecla soltada");
-// });
+  // input.addEventListener('keyup', function (e) {
+  //     console.log("Tecla soltada");
+  // });
 
-// Trabajando sobre formulario
-const form = document.getElementById('create-course');
+  // Trabajando sobre formulario
+  const form = document.getElementById("create-course");
 
-form.addEventListener('submit', (e) => {
+  form.addEventListener("submit", (e) => {
     e.preventDefault();
 
-    let title = document.getElementById('title-form').value;
-    let description = document.getElementById('description-form').value;
+    let title = document.getElementById("title-form").value;
+    let description = document.getElementById("description-form").value;
 
     console.log(title + "\n" + description);
+  });
+
+  const checkbox = document.getElementById("checkbox");
+
+  checkbox.addEventListener("change", (e) => {
+    console.log(e.target.checked);
+  });
+
+  const titleForm = document.getElementById("title-form");
+
+  titleForm.addEventListener("change", (e) => {
+    console.dir(e.target.checked);
+  });
 });
